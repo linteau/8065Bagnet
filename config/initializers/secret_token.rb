6bbc53cfd949a7aacca212f89974fcf1b49e4ccb
andylinteau <andy.linteau@buystand.com>
2013-09-11 02:03:22 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coffee8065::Application.config.secret_token = 'b0b8a6a16a99688ed6b7a62253db173aa32dcac60caed90aa21bfbcf01b111f93d861e01850b06836de90f43275233d5efb513f9881424a007b533eca007aa3d'
