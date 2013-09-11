48bb1f9558c8f3a1c07ef64f001d566139405d6d
andylinteau <andy.linteau@buystand.com>
2013-09-11 11:44:11 +0800

    style home page and add content to menu page

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -11,11 +11,13 @@ describe "Pages" do
     it { should have_selector('h1', text: "Home" )}
     
     describe "have links" do
-      it { should have_link('Home')}
-      it { should have_link('Menu')}
-      it { should have_link('About')}
-      it { should have_link('Contact')}
-      it { should have_link('Hours')}      
+      it { should have_link('Home') }
+      it { should have_link('Menu') }
+      it { should have_link('About') }
+      it { should have_link('Contact') }
+      it { should have_link('Hours') } 
+      it { should have_link('Coffee 8065') }      
+           
     
     end
   end
